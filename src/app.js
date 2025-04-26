02277ab68f6cd1d8d7a12d531475bc92a2b1ec6a
kgronner1 <56982452+kgronner1@users.noreply.github.com>
2025-04-26 10:30:02 -0700

    app.js
    pure networking rift-brain -- working in April 2025

--- src/app.js
+++ src/app.js
@@ -10,9 +10,6 @@
 
 ////// TO DO ///////
 
-// ssh -i ./Projects/AWS-Rift/godot-multiplayer-server-test.pem ec2-user@13.57.47.91 
-// scp -i ./Projects/AWS-Rift/godot-multiplayer-server-test.pem -l 20000 ./Projects/Rift-Brain/src/app.js ec2-user@13.57.47.91:/home/ec2-user/Rift-Brain/src/
-
 var express = require('express');
 var app = express();
 
@@ -27,6 +24,10 @@ const PORTS =  [8080, 8081, 8082, 8083, 8084, 8085];
 
 const MAX_PLAYERS = 4;
 
+// 1:30 is 90,000
+const GAME_HEALTH_TIME = 90000;
+
+// master game instance object
 var game_instances = {};
 
 // we want this one listening on port 3000
@@ -38,7 +39,8 @@ app.listen(3000, function () {
 
 async function runCommand(command, args = [], cwd = '/home/ec2-user/') {
   return new Promise((resolve, reject) => {
-      const child = spawn(command, args, { stdio: 'inherit', cwd });
+      console.log(command, args);
+      const child = spawn(command, args, { stdio: 'inherit', cwd, shell:true });
 
       child.on('spawn', () => {
           console.log("RESOLVE runCommand");
@@ -53,6 +55,97 @@ async function runCommand(command, args = [], cwd = '/home/ec2-user/') {
 }
 
 
+// kill the process and the health check timer for a game_instance
+async function endGameInstance(game_instance) {
+
+  // run the script to end the game instance
+  console.log("BEFORE ENDGAME");
+
+  // run this script for the next port that doesn't have a game
+  let command = "kill $(lsof -t -i:" + game_instance + ")";
+
+  console.log("runEndCommand: ", command);
+  // await completion
+  await runCommand(command);
+
+  console.log("AFTER runCommand");
+
+  // before deleting the game object, clear the time out
+  if (game_instances[game_instance]) {
+    if (game_instances[game_instance].timer) {
+      clearTimeout(game_instances[game_instance].timer);
+      console.log("called to remove timer from endGameInstance:", game_instance);
+    }
+  }
+
+  // remove the game instance from game_instances from the object
+  delete game_instances[game_instance];
+
+  return;
+}
+
+
+// this will create a new game instance and store them into the game_instances object
+// returns the new game instance port or 1 for wait
+async function createGameInstance(private_code = "") {
+  // the default return is a wait code of 1
+  let new_game_instance_port = 1;
+
+  // find the next port from possible PORTS list that does not already have a game instance
+  let unused_ports = PORTS.filter((a) => !game_instances.hasOwnProperty(a));
+
+  if (unused_ports.length > 0) {
+      new_game_instance_port = unused_ports[0];
+
+      console.log("BEFORE CREATE GAME");
+      // run this script for the next port that doesn't have a game
+      let command = "/home/ec2-user/rift_jumper_multiplayer_server_test.x86_64";
+
+      let options = []
+      options.push(`--port=${new_game_instance_port}`);
+      if (private_code) {
+        options.push(` --private_code=${private_code}`);
+        console.log("runcom options:", options);
+      }
+
+      await runCommand(command, options);
+  } else {
+      return 1;
+  }
+
+  // game_instance object:
+  // game_instance[PORTNUM] = {"players":0,"active":false, "healthy":false, "private":false, "private_code":0}
+  // players: how many
+  // active: the game is mid-session and players are playing
+  // healthy: this server is alive and well
+  // private: need a game code to join
+  // private_code: host lobbies have a password
+
+  // Initialize the game instance object
+  game_instances[new_game_instance_port] = {
+      players: 0,
+      active: false,
+      healthy: false,
+      private: false,
+      private_code: "0",
+      timer: null,
+  };
+
+  console.log("game_instances", game_instances);
+
+  // Watch the "healthy" property
+  return watchProperty(game_instances[new_game_instance_port], "healthy", 5000, false).then((healthyValue) => {
+      
+      // begin the health timer
+      startHealthCheckTimer(new_game_instance_port);
+
+      console.log("Healthy value:", healthyValue);
+      console.log("new_game_instance_port:", new_game_instance_port);
+
+      // Conditionally return based on healthyValue
+      return healthyValue ? new_game_instance_port : 1;
+  });
+}
 
 // watch a specific property and if it changes before the timeout, return the new value
 function watchProperty(obj, property, timeout, defaultValue) {
@@ -80,82 +173,32 @@ function watchProperty(obj, property, timeout, defaultValue) {
   });
 }
 
-// this will create a new game instance and store them into the game_instances object
-// returns the new game instance port or 1 for wait
-async function createGameInstance() {
-  // the default return is a wait code of 1
-  let new_game_instance_port = 1;
-
-  // find the next port from possible PORTS list that does not already have a game instance
-  let unused_ports = PORTS.filter((a) => !game_instances.hasOwnProperty(a));
-
-  if (unused_ports.length > 0) {
-      new_game_instance_port = unused_ports[0];
-
-      console.log("BEFORE runCommand");
-      // run this script for the next port that doesn't have a game
-      let command = "/home/ec2-user/rift_jumper_multiplayer_server_test.x86_64";
-
-      await runCommand(command, [`--port=${new_game_instance_port}`]);
-  } else {
-      return 1;
-  }
-
-
-  // game_instance object:
-  // game_instance[PORTNUM] = {"players":0,"active":false, "healthy":false, "private":false, "private_code":0}
-  // players: how many
-  // active: the game is mid-session and players are playing
-  // healthy: this server is alive and well
-  // private: need a game code to join
-  // private_code: host lobbies have a password
-
-  // Initialize the game instance object
-  game_instances[new_game_instance_port] = {
-      players: 0,
-      active: false,
-      healthy: false,
-      private: false,
-      private_code: "0",
-  };
-
-  console.log("game_instances", game_instances);
-
-  // Watch the "healthy" property
-  return watchProperty(game_instances[new_game_instance_port], "healthy", 5000, false).then((healthyValue) => {
-      
-      console.log("Healthy value:", healthyValue);
-      console.log("new_game_instance_port:", new_game_instance_port);
-
-      // Conditionally return based on healthyValue
-      return healthyValue ? new_game_instance_port : 1;
-  });
-}
-
-
-function objectChanged() {
-  // great
-}
-
-// kill the process and the health check timer for a game_instance
-function endGameInstance(game_instance) {
-    // run the script to end the game instance
-    // await completion
-    // kill the timer
-    // remove the game instance from game_instances
-    return;
-}
 
 // called after the godot instances are created
-function startHealthCheck() {
+function startHealthCheckTimer(game_port) {
 
-  // note there is also a timer on the game_instance godot side that pings the healthcheck endpoint below every thirty seconds
+  // no game_port? its already ended
+  if (!game_instances[game_port]) {
+    // run kill instance for good measure
+    console.log("ending game instance call from startHealthCheclTimer no game_port in obj");
+    endGameInstance(game_port);
+    return;
+  }
 
-  // set a timer
+  // clear existing timer if it exists
+  if (game_instances[game_port].timer) {
+      clearTimeout(game_instances[game_port].timer);
+  }
 
-  // the timer is 1:30 and reset when health check endpoint is hit
+  console.log(`Starting timer for game instance ${game_port}`);
 
-  // if the timer goes off it triggers a shutdown from the linux side of the port game instance
+  // set a new timeout for const health time
+  // the timer is reset when health check endpoint is hit
+  game_instances[game_port].timer = setTimeout(() => {
+      // if the timer goes off it triggers a shutdown from the linux side of the port game instance
+      console.log("ending game instance call from,"+game_port+" timer ran out");
+      endGameInstance(game_port);
+  }, GAME_HEALTH_TIME);
 
 }
 
@@ -168,7 +211,7 @@ function checkForJoinablePrivateGame(player_submitted_private_code) {
     
     let game_port = Object.keys(game)[0];
 
-    // if 
+    // do we have any games with the private code
     if (!Object.keys(game)[0]) {
 
       // this game doesn't exist
@@ -220,22 +263,22 @@ app.get('/join', async function (req, res) {
 
   if (create_private_game) {
 
-    game_port = await createGameInstance();
+    // create a host code before creating game so we can pass it as needed
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let code = '';
+    for (let i = 0;i < 4; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      code += characters[randomIndex];
+    }
+
+    let private_code = code;
+
+    game_port = await createGameInstance(private_code);
 
     // if its not a wait code or a reject
     if (game_port > 1) {
       // set it to private
       game_instances[game_port]["private"] = true;
-
-      // create a host code 
-      const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      let code = '';
-      for (let i = 0;i < 5; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        code += characters[randomIndex];
-      }
-
-      private_code = code;
 
       // add the host code
       game_instances[game_port]["private_code"] = private_code;
@@ -301,30 +344,26 @@ app.get('/join', async function (req, res) {
 
 // // // // // // // // // // // // // game instance response api // // // // // // // // // // // // //
 
-// to access these endpoints you need to be on the same server? is that possible?
+// to access these endpoints you need to be on the same server
 
 // must always pass as queries:
 // game_instance=0000
 
 app.get('/health_check', function (req, res) {
-
+  
   // what game instance is this? this must be passed as a query
   let passed_game_instance = req.query.game_instance;
 
-  let passed_player_count = req.query.player_count;
+  console.log("HEALTHY CHECK hit: ", passed_game_instance);
+  console.log("All game instances: ", game_instances);
 
-  let passed_healthy = req.query.healthy;
-
-  let passed_active = req.query.active;
-
-  if (passed_game_instance && passed_player_count &&  passed_healthy && passed_active && game_instances.hasOwnProperty(passed_game_instance)) {
-    // set with the latest data
-    game_instances[passed_game_instance]["players"] = passed_player_count;
-    game_instances[passed_game_instance]["healthy"] = passed_healthy;
-    game_instances[passed_game_instance]["active"] = passed_active;
+  if (passed_game_instance && game_instances.hasOwnProperty(passed_game_instance)) {
+    // reset the health timer by starting it again
+    startHealthCheckTimer(passed_game_instance);
   }
   else if (passed_game_instance) {
-    // kill the process on that port and the timer asking for health checks
+    // kill the process on that port and delete the game_instance object
+    console.log("ending game instance call from /health_check");
     endGameInstance(passed_game_instance);
   }
   else {
@@ -336,15 +375,19 @@ app.get('/health_check', function (req, res) {
 
 app.get('/player_left_instance', function (req, res) {
 
+  console.log("pre-player left: ");
+
   // what game instance is this? this must be passed as a query
   let passed_game_instance = req.query.game_instance;
 
   if (passed_game_instance && game_instances.hasOwnProperty(passed_game_instance)) {
     if (game_instances[passed_game_instance]["players"] > 1) {
       // remove a player from the count
+      console.log("minus one player");
       game_instances[passed_game_instance]["players"]--;
     }
     else {
+      console.log("ending game instance call from player_left_instance");
       endGameInstance(passed_game_instance);
     }
   }
@@ -362,8 +405,10 @@ app.get('/player_joined_instance', function (req, res) {
   // what game instance is this? this must be passed as a query
   let passed_game_instance = req.query.game_instance;
 
+  console.log("pre-player joined: ", game_instances[passed_game_instance]["players"]);
   // add a player
   game_instances[passed_game_instance]["players"]++;
+  console.log("post-player joined: ", game_instances[passed_game_instance]["players"]);
 
   //res.status(200).send(JSON.stringify(x));
 });
@@ -388,7 +433,6 @@ app.get('/game_ended', function (req, res) {
 
 app.get('/game_instance_ready', function (req, res) {
 
-  console.log("GAME IS READY");
   // what game instance is this? this must be passed as a query
   let passed_game_instance = req.query.game_instance;
 
